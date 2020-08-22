399ceb217d2873db88c9a9d69d715eb4b5de28ae
Cem Yıldız <cem.yildiz@safrantek.com>
2020-08-22 03:15:28 +0300

    .

--- RastgeleSayi/Controllers/Number.cs
+++ RastgeleSayi/Controllers/Number.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace RastgeleSayi.Controllers
 {
